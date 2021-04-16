727eda337082b1399401d06c38d13f37b470a999
Yurun <admin@yurunsoft.com>
2021-04-16 17:34:08 +0800

    Github Actions 测试迁移

---
+++ .github/prepare-test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+__DIR__=$(cd `dirname $0`; pwd)
+
+cd $__DIR__
+
+containerName=$1
+
+docker-compose up -d $containerName \
+&& docker exec $containerName php -v \
+&& docker exec $containerName php -m \
+&& docker exec $containerName composer -V \
+&& docker ps -a
+
+n=0
+until [ $n -ge 5 ]
+do
+  docker exec $containerName composer update && break
+  n=$[$n+1]
+  sleep 1
+done
